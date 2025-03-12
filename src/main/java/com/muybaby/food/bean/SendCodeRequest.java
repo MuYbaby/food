a7240d318ccda25874d304390dd78841b7a0e0c7
MuYbaby <45114339+MuYbaby@users.noreply.github.com>
2025-03-12 15:17:27 +0800

    Delete src/main directory

--- src/main/java/com/muybaby/food/bean/SendCodeRequest.java
+++
@@ -1,8 +0,0 @@
-package com.muybaby.food.bean;
-
-import lombok.Data;
-
-@Data
-public class SendCodeRequest {
-    private String email;
-}
--- src/main/java/com/muybaby/food/exception/GlobalExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.muybaby.food.exception;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-
-}
